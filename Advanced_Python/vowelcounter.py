0fdf399f9d12b297c86029c727848800e3d9086f
mahmooooodie <139579599+mahmooooodie@users.noreply.github.com>
2023-11-02 14:06:34 +0300

    Advanced Python Demo
    demonstration of my advance python expertise

---
+++ Advanced_Python/vowelcounter.py
@@ -0,0 +1,29 @@
+class vowelcounter:
+    def __init__(self):
+        self.sesli_harfler = "aeıioöuü"
+        self.sayac = 0
+
+    def kelime_sor(self):
+        return input("Bir kelime giriniz: ").lower()
+    
+    def seslidir(self, harf):
+        return harf in self.sesli_harfler
+    
+    def artir(self):
+        for harf in self.kelime:
+            if self.seslidir(harf):
+                self.sayac += 1
+        return self.sayac
+    
+    def ekrana_goster(self):
+        sesli_harf_sayisi = self.artir()
+        mesaj = f"the word {self.kelime} has {sesli_harf_sayisi} vowels."
+        print(mesaj)
+
+    def calistir(self):
+        self.kelime = self.kelime_sor()
+        self.ekrana_goster()
+
+if __name__ == '__main__':
+    sayac = vowelcounter()
+    sayac.calistir()
